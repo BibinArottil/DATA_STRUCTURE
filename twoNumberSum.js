2e371b7501abbd26e26b72b866eae3aaa2057deb
Bibin A <bibinolavanna@gmail.com>
2023-05-15 14:24:58 +0530

    initial commit

---
+++ twoNumberSum.js
@@ -0,0 +1,19 @@
+let array = [6, 5, 4, 3, 8, 0, 1];
+let target = 10;
+let result = [];
+
+function twoNumberSum(array, target) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] == target) {
+        return result.push(array[i], array[j]);
+      }
+    }
+  }
+}
+
+twoNumberSum(array, target);
+
+for (let i = 0; i < result.length; i++) {
+  console.log(result[i]);
+}

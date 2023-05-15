4f1b95af023d1f40d2085dd61be4811f5d276067
Bibin A <bibinolavanna@gmail.com>
2023-05-15 19:14:53 +0530

    value added

--- factorial.js
+++ factorial.js
@@ -8,5 +8,6 @@ function factorial(n){
   
   console.log(factorial(0)) // 1
   console.log(factorial(1)) // 1
+  console.log(factorial(3)) // 6
   console.log(factorial(4)) // 24
   console.log(factorial(5)) // 120

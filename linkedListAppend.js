e8c68bdb2904cc35046a2676e10017756b77f0d7
Bibin A <bibinolavanna@gmail.com>
2023-05-16 12:31:53 +0530

    Append

---
+++ linkedListAppend.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
+    }
+    this.size++;
+  }
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
+}
+
+const list = new LinkedList();
+console.log("List is empty", list.isEmpty()); // true
+console.log("List size", list.getSize()); // 0
+list.print(); // List is empty
+
+list.append(10);
+list.print(); // 10
+list.append(20);
+list.append(30);
+list.print(); // 10 20 30

53c62cf03141d5c19d42640edadd5f25324bc615
Bibin A <bibinolavanna@gmail.com>
2023-05-16 12:17:53 +0530

    Create a list

---
+++ linkedList.js
@@ -0,0 +1,26 @@
+// Create a linked list
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+
+const list = new LinkedList();
+console.log("List is empty", list.isEmpty()); // true
+console.log("List size", list.getSize()); // 0

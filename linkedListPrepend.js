f1b545da535b21ec521d175661f9bca35091756f
Bibin A <bibinolavanna@gmail.com>
2023-05-16 12:21:09 +0530

    Prepend

---
+++ linkedListPrepend.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
+}
+
+const list = new LinkedList();
+console.log("List is empty", list.isEmpty()); // true
+console.log("List size", list.getSize()); // 0
+list.print(); // List is empty
+
+list.prepend(10);
+list.print(); // 10
+list.prepend(20);
+list.prepend(30);
+list.print(); // 30 20 10

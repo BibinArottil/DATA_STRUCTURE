4d82c8afe4e9ddd06eb65ef982ef93f4a1ae0f6e
Bibin A <bibinolavanna@gmail.com>
2023-05-20 10:14:16 +0530

    Bubble sort

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+
+const arr = [3, -2, 5, 6, 4, 7];
+bubbleSort(arr);
+console.log(arr); // [-2,3,4,5,6,7]

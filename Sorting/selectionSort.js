a05818089e145550d4642e479aae810121636a4a
Bibin A <bibinolavanna@gmail.com>
2023-05-21 16:36:29 +0530

    Selection sort

---
+++ Sorting/selectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return console.log(arr);
+}
+
+selectionSort([34, 22, 10, 19, 17]);
